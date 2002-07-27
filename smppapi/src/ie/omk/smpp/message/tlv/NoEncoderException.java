61fe574f766a877c237fba47086b676c08c44056
Oran Kelly <oranoceallaigh@smppapi.git>
2002-07-27 14:53:24 +0000

    Add Javadoc

--- smppapi/src/ie/omk/smpp/message/tlv/NoEncoderException.java
+++ smppapi/src/ie/omk/smpp/message/tlv/NoEncoderException.java
@@ -24,12 +24,29 @@
  */
 package ie.omk.smpp.message.tlv;
 
+/** No encoder found for a Java type. An attempt was made to define a new tag
+ * with a value type that the API does not have a known encoder for. The
+ * application should define a new encoder and define the tag passing that
+ * encoder to the
+ * {@link ie.omk.smpp.message.tlv.Tag#defineTag(int, java.lang.Class, ie.omk.smpp.message.tlv.Encoder, int)}
+ * method.
+ * @author Oran Kelly &lt;orank@users.sf.net&gt;
+ */
 public class NoEncoderException extends RuntimeException {
     
-    public NoEncoderException() {
+    public Class type = null;
+
+    /** Create a new NoEncoderException.
+     * @param type The Java type that no encoder was found for.
+     */
+    public NoEncoderException(java.lang.Class type) {
     }
 
-    public NoEncoderException(String msg) {
+    /** Create a new NoEncoderException.
+     * @param type The Java type that no encoder was found for.
+     * @param msg The exception message.
+     */
+    public NoEncoderException(java.lang.Class type, String msg) {
 	super (msg);
     }
 }
--- smppapi/src/ie/omk/smpp/message/tlv/TagDefinedException.java
+++ smppapi/src/ie/omk/smpp/message/tlv/TagDefinedException.java
@@ -24,22 +24,37 @@
  */
 package ie.omk.smpp.message.tlv;
 
+
+/** Tag has already been defined. This exception is thrown by
+ * {@link Tag#defineTag} if an attempt is made to redefine a tag which already
+ * has a definition.
+ * @author Oran Kelly &lt;orank@users.sf.net&gt;
+ */
 public class TagDefinedException extends java.lang.RuntimeException
 {
+    /** The value of the tag that was attempted to be redefined.
+     */    
     private int tagValue = -1;
 
-    public TagDefinedException() {
+    /** Create a new TagDefinedException.
+     * @param tagValue The tag for which an attempt as made to redefine.
+     */
+    public TagDefinedException(int tagValue) {
+	this.tagValue = tagValue;
     }
 
-    public TagDefinedException(String msg) {
-	super (msg);
-    }
-
-    public TagDefinedException(String msg, int tagValue) {
+    /** Create a new TagDefinedException.
+     * @param tagValue The tag for which an attempt as made to redefine.
+     * @param msg The exception message.
+     */
+    public TagDefinedException(int tagValue, String msg) {
 	super (msg);
 	this.tagValue = tagValue;
     }
 
+    /** Get the tag for which an attempt as made to redefine.
+     * @return The tag for which an attempt as made to redefine.
+     */
     public int getTagValue() {
 	return (tagValue);
     }
