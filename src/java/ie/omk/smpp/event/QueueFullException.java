7f5ca544d81d3cbdf1c9c0d3101f1c699ffde17c
Oran Kelly <oranoceallaigh@smppapi.git>
2006-03-09 13:18:08 +0000

    Add standard constructors from parent class.

--- src/java/ie/omk/smpp/event/QueueFullException.java
+++ src/java/ie/omk/smpp/event/QueueFullException.java
@@ -2,4 +2,20 @@ package ie.omk.smpp.event;
 
 public class QueueFullException extends RuntimeException {
     static final long serialVersionUID = 1L;
+
+    public QueueFullException() {
+        super();
+    }
+
+    public QueueFullException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public QueueFullException(String message) {
+        super(message);
+    }
+
+    public QueueFullException(Throwable cause) {
+        super(cause);
+    }
 }
