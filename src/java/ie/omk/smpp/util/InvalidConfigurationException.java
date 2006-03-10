d0221da8da8a2c96e72a92aeb0112504269c4ca2
Oran Kelly <oranoceallaigh@smppapi.git>
2006-03-10 14:05:30 +0000

    Change the constructors of this class, add the value property.

--- src/java/ie/omk/smpp/util/InvalidConfigurationException.java
+++ src/java/ie/omk/smpp/util/InvalidConfigurationException.java
@@ -4,18 +4,24 @@ public class InvalidConfigurationException extends
         ie.omk.smpp.SMPPRuntimeException {
     static final long serialVersionUID = 5616081756943055309L;
     
-    private String property = "";
-
-    public InvalidConfigurationException() {
-    }
-
-    public InvalidConfigurationException(String msg) {
-        super(msg);
-    }
+    private final String property;
+    private final String value;
 
     public InvalidConfigurationException(String msg, String property) {
         super(msg);
         this.property = property;
+        this.value = null;
+    }
+    public InvalidConfigurationException(String msg, String property, String value) {
+        super(msg);
+        this.property = property;
+        this.value = value;
+    }
+    public InvalidConfigurationException(
+            String msg, String property, Throwable rootCause) {
+        super(msg, rootCause);
+        this.property = property;
+        this.value = null;
     }
 
     /**
@@ -23,6 +29,14 @@ public class InvalidConfigurationException extends
      */
     public String getProperty() {
         return property;
+    }
+    
+    /**
+     * Get the value which caused this exception.
+     * @return The offending value.
+     */
+    public String getValue() {
+        return value;
     }
 }
 
