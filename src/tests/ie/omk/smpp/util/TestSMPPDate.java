17cf3587ac3801372184750c2d2e05d8b50bcefe
Oran Kelly <oranoceallaigh@smppapi.git>
2006-03-10 16:20:17 +0000

    Flesh out and improve the SMPPDate unit tests.

--- src/tests/ie/omk/smpp/util/TestSMPPDate.java
+++ src/tests/ie/omk/smpp/util/TestSMPPDate.java
@@ -2,101 +2,19 @@ package ie.omk.smpp.util;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
 import junit.framework.TestCase;
 
 public class TestSMPPDate extends TestCase {
 
-    protected Calendar a_testCalendar = null;
-
-    protected SMPPDate a_testDate = null;
-
-    protected SMPPDate r_testDate = null;
-
-    protected int a_year = 2002;
-
-    protected int a_month = Calendar.MAY;
-
-    protected int a_day = 7;
-
-    protected int a_hour = 14;
-
-    protected int a_minute = 34;
-
-    protected int a_second = 33;
-
-    protected int a_tenth = 400;
-
-    protected int a_offset = -1;
-
-    protected TimeZone tz = TimeZone.getTimeZone("Asia/Calcutta");
-
-    protected int r_years = 1;
-
-    protected int r_months = 0;
-
-    protected int r_days = 5;
-
-    protected int r_hours = 2;
-
-    protected int r_minutes = 45;
-
-    protected int r_seconds = 12;
-
-    // This string will be completed in setUp with the UTC offset info
-    protected String a_expectedDateString = "0205071434334";
-
-    protected String r_expectedDateString = "010005024512000R";
-
-    public TestSMPPDate() {
-        super("ie.omk.smpp.util.SMPPDate test suite");
-    }
-
-    public TestSMPPDate(String name) {
-        super(name);
-    }
-
-    protected void setUp() {
-        a_testCalendar = Calendar.getInstance();
-
-        char sign = '+';
-        int offset = tz.getRawOffset();
-        if (offset < 0) {
-            sign = '-';
-        }
-
-        offset = Math.abs(offset) / 900000;
-
-        a_testCalendar.setTimeZone(tz);
-        a_testCalendar.set(Calendar.YEAR, a_year);
-        a_testCalendar.set(Calendar.MONTH, a_month);
-        a_testCalendar.set(Calendar.DAY_OF_MONTH, a_day);
-        a_testCalendar.set(Calendar.HOUR_OF_DAY, a_hour);
-        a_testCalendar.set(Calendar.MINUTE, a_minute);
-        a_testCalendar.set(Calendar.SECOND, a_second);
-        a_testCalendar.set(Calendar.MILLISECOND, a_tenth);
-
-        StringBuffer b = new StringBuffer(a_expectedDateString);
-        if (offset < 10) {
-            b.append('0');
-        }
-
-        b.append(Integer.toString(offset)).append(sign);
-        a_expectedDateString = b.toString();
-
-        a_testDate = new SMPPDate(a_testCalendar);
-        r_testDate = new SMPPDate(r_years, r_months, r_days, r_hours,
-                r_minutes, r_seconds);
-    }
-
-    protected void tearDown() throws Exception {
-    }
+    private final TimeZone calcuttaTimeZone = TimeZone.getTimeZone("Asia/Calcutta");
 
     public void testDefaultConstructor() {
         SMPPDate d = new SMPPDate();
 
-        assertTrue(!d.isRelative());
+        assertFalse(d.isRelative());
         assertEquals(0, d.getYear());
         assertEquals(0, d.getMonth());
         assertEquals(0, d.getDay());
@@ -109,65 +27,170 @@ public class TestSMPPDate extends TestCase {
     }
 
     public void testDateConstructor() {
-        Date date = a_testCalendar.getTime();
-        SMPPDate d = new SMPPDate(date);
-
-        // Need to do a comparison that is independant of time zone
-        // dependancies.
-        long l1 = date.getTime();
-        long l2 = d.getCalendar().getTimeInMillis();
-        assertEquals(l1, l2);
+        Date now = new Date();
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(now);
+        TimeZone localZone = TimeZone.getDefault();
+        
+        SMPPDate d = new SMPPDate(now);
+        assertEquals(cal.get(Calendar.YEAR) - 2000, d.getYear());
+        assertEquals(cal.get(Calendar.MONTH) + 1, d.getMonth());
+        assertEquals(cal.get(Calendar.DAY_OF_MONTH), d.getDay());
+        assertEquals(cal.get(Calendar.HOUR_OF_DAY), d.getHour());
+        assertEquals(cal.get(Calendar.MINUTE), d.getMinute());
+        assertEquals(cal.get(Calendar.SECOND), d.getSecond());
+        assertEquals(cal.get(Calendar.MILLISECOND) / 100, d.getTenth());
+        assertEquals(cal.getTimeZone(), d.getTimeZone());
+        if (localZone.getRawOffset() < 0) {
+            assertEquals('-', d.getSign());
+        } else {
+            assertEquals('+', d.getSign());
+        }
     }
 
     public void testCalendarConstructor() {
-        SMPPDate d = new SMPPDate(a_testCalendar);
-        assertEquals(a_expectedDateString, d.toString());
-        assertEquals(a_expectedDateString.hashCode(), d.hashCode());
+        Calendar cal = new GregorianCalendar();
+        SMPPDate d = new SMPPDate(cal);
+        assertEquals(cal.get(Calendar.YEAR) - 2000, d.getYear());
+        assertEquals(cal.get(Calendar.MONTH) + 1, d.getMonth());
+        assertEquals(cal.get(Calendar.DAY_OF_MONTH), d.getDay());
+        assertEquals(cal.get(Calendar.HOUR_OF_DAY), d.getHour());
+        assertEquals(cal.get(Calendar.MINUTE), d.getMinute());
+        assertEquals(cal.get(Calendar.SECOND), d.getSecond());
+        assertEquals(cal.get(Calendar.MILLISECOND) / 100, d.getTenth());
+        assertEquals(cal.getTimeZone(), d.getTimeZone());
+        if (cal.getTimeZone().getRawOffset() < 0) {
+            assertEquals('-', d.getSign());
+        } else {
+            assertEquals('+', d.getSign());
+        }
     }
 
     public void testRelativeConstructor() {
-        SMPPDate d = new SMPPDate(r_years, r_months, r_days, r_hours,
-                r_minutes, r_seconds);
+        SMPPDate d = new SMPPDate(2, 5, 12, 9, 55, 3);
         assertTrue(d.isRelative());
-        assertEquals("String compare", r_expectedDateString, d.toString());
-        assertEquals("HashCode compare", r_expectedDateString.hashCode(), d
-                .hashCode());
+        assertEquals('R', d.getSign());
+        assertEquals(2, d.getYear());
+        assertEquals(5, d.getMonth());
+        assertEquals(12, d.getDay());
+        assertEquals(9, d.getHour());
+        assertEquals(55, d.getMinute());
+        assertEquals(3, d.getSecond());
+        assertEquals(0, d.getTenth());
+        assertEquals(0, d.getUtcOffset());
+        assertNull(d.getTimeZone());
     }
 
-    public void testEquality() {
-        SMPPDate d1 = new SMPPDate(a_testCalendar);
-        SMPPDate d2 = new SMPPDate(a_testCalendar);
+    public void testEqualsAndHashCodeWithDefaultTimeZone() {
+        Calendar now = new GregorianCalendar();
+        Calendar other = new GregorianCalendar();
+        other.set(Calendar.YEAR, 1956);
+        SMPPDate d1 = new SMPPDate(now);
+        SMPPDate d2 = new SMPPDate(now);
+        SMPPDate d3 = new SMPPDate(other);
+        assertEquals(d1, d1);
+        assertEquals(d2, d2);
         assertEquals(d1, d2);
+        assertEquals(d2, d1);
+        assertEquals(d1.hashCode(), d2.hashCode());
+        assertFalse(d1.equals(d3));
+        assertFalse(d2.equals(d3));
+        assertFalse(d3.equals(d1));
+        assertFalse(d3.equals(d2));
+        assertFalse(d1.hashCode() == d3.hashCode());
+        assertFalse(d2.hashCode() == d3.hashCode());
 
-        d1 = new SMPPDate(a_testCalendar.getTime());
-        d2 = new SMPPDate(a_testCalendar.getTime());
+        // Use the java.util.Date constructor
+        d2 = new SMPPDate(now.getTime());
+        assertEquals(d2, d2);
         assertEquals(d1, d2);
-
-        SMPPDate d3 = new SMPPDate();
-        assertTrue(!d3.equals(d1));
-        assertTrue(!d3.equals(d2));
+        assertEquals(d2, d1);
+        assertEquals(d1.hashCode(), d2.hashCode());
+    }
+    
+    public void testEqualsAndHashCodeWithExplicitTimeZone() {
+        Date now = new Date();
+        Calendar nowLocal = Calendar.getInstance();
+        nowLocal.setTime(now);
+        
+        Calendar nowElseWhere = Calendar.getInstance();
+        nowElseWhere.setTimeZone(calcuttaTimeZone);
+        nowElseWhere.setTime(now);
+        
+        SMPPDate d1 = new SMPPDate(nowLocal);
+        SMPPDate d2 = new SMPPDate(nowElseWhere);
+        assertFalse(d1.equals(d2));
+        assertFalse(d2.equals(d1));
+        assertFalse(d1.hashCode() == d2.hashCode());
     }
 
-    public void testParsing() {
+    public void testParseSMPPDate() {
         try {
-            SMPPDate d1 = SMPPDate.parseSMPPDate(a_expectedDateString);
-            SMPPDate d2 = SMPPDate.parseSMPPDate(r_expectedDateString);
+            final String absolute1 = "050504123211808+";
+            final String absolute2 = "050504123211812-";
+            final String relative = "000000040559000R";
+            
+            final Calendar absCalendar1 = Calendar.getInstance();
+            absCalendar1.setTimeZone(TimeZone.getTimeZone("Asia/Beirut"));
+            absCalendar1.set(Calendar.YEAR, 2005);
+            absCalendar1.set(Calendar.MONTH, 4);
+            absCalendar1.set(Calendar.DAY_OF_MONTH, 4);
+            absCalendar1.set(Calendar.HOUR_OF_DAY, 12);
+            absCalendar1.set(Calendar.MINUTE, 32);
+            absCalendar1.set(Calendar.SECOND, 11);
+            absCalendar1.set(Calendar.MILLISECOND, 800);
 
-            assertEquals("Absolute doesn't match", a_testDate, d1);
-            assertEquals("Absolute hashCode doesn't match", a_testDate
-                    .hashCode(), d1.hashCode());
+            final Calendar absCalendar2 = Calendar.getInstance();
+            absCalendar2.setTimeZone(TimeZone.getTimeZone("America/Argentina/Buenos_Aires"));
+            absCalendar2.set(Calendar.YEAR, 2005);
+            absCalendar2.set(Calendar.MONTH, 4);
+            absCalendar2.set(Calendar.DAY_OF_MONTH, 4);
+            absCalendar2.set(Calendar.HOUR_OF_DAY, 12);
+            absCalendar2.set(Calendar.MINUTE, 32);
+            absCalendar2.set(Calendar.SECOND, 11);
+            absCalendar2.set(Calendar.MILLISECOND, 800);
+            
+            SMPPDate absDate1 = SMPPDate.parseSMPPDate(absolute1);
+            assertEquals(absCalendar1.getTime(), absDate1.getCalendar().getTime());
+            assertNotNull(absDate1.getTimeZone());
+            assertEquals(2 * 3600000, absDate1.getTimeZone().getRawOffset());
 
-            assertEquals("Relative doesn't match", r_testDate, d2);
-            assertEquals("Relative hashCode doesn't match", r_testDate
-                    .hashCode(), d2.hashCode());
+            SMPPDate absDate2 = SMPPDate.parseSMPPDate(absolute2);
+            assertEquals(absCalendar2.getTime(), absDate2.getCalendar().getTime());
+            assertNotNull(absDate2.getTimeZone());
+            assertEquals(3 * -3600000, absDate2.getTimeZone().getRawOffset());
+
+            SMPPDate relDate = SMPPDate.parseSMPPDate(relative);
+            assertEquals(0, relDate.getYear());
+            assertEquals(0, relDate.getMonth());
+            assertEquals(0, relDate.getDay());
+            assertEquals(4, relDate.getHour());
+            assertEquals(5, relDate.getMinute());
+            assertEquals(59, relDate.getSecond());
+            assertEquals(0, relDate.getTenth());
+            assertEquals(0, relDate.getUtcOffset());
+            assertNull(relDate.getTimeZone());
         } catch (InvalidDateFormatException x) {
             fail("Bad date format: " + x.toString());
         }
     }
 
-    public void testSerializing() {
-        assertEquals(a_expectedDateString, a_testDate.toString());
-        assertEquals(r_expectedDateString, r_testDate.toString());
+    public void testToString() {
+        final String absExpected = "060913165844422+";
+        final Calendar cal = Calendar.getInstance();
+        cal.setTimeZone(calcuttaTimeZone);
+        cal.set(Calendar.YEAR, 2006);
+        cal.set(Calendar.MONTH, 8);
+        cal.set(Calendar.DAY_OF_MONTH, 13);
+        cal.set(Calendar.HOUR_OF_DAY, 16);
+        cal.set(Calendar.MINUTE, 58);
+        cal.set(Calendar.SECOND, 44);
+        cal.set(Calendar.MILLISECOND, 499);
+        SMPPDate smppDate = new SMPPDate(cal);
+        assertEquals(absExpected, smppDate.toString());
+        
+        final String relExpected = "010003041320000R";
+        smppDate = new SMPPDate(1, 0, 3, 4, 13, 20);
+        assertEquals(relExpected, smppDate.toString());
     }
 }
-
