ffbf5425c6ebe35f213dc874cf2fcf69e49aaca5
Oran Kelly <oranoceallaigh@smppapi.git>
2007-02-06 20:51:12 +0000

    Update test and add some explanatory comments.

--- src/tests/ie/omk/smpp/util/DefaultAlphabetEncodingTest.java
+++ src/tests/ie/omk/smpp/util/DefaultAlphabetEncodingTest.java
@@ -13,13 +13,14 @@ public class DefaultAlphabetEncodingTest extends TestCase {
         new DefaultAlphabetEncoding();
     
     public void testDefaultAlphabet() {
-        // 127 characters in the base table, and 2 bytes for each character
-        // in the extended table.
-        final int expectedEncodedLength = 127 + (2 * 9);
+        // 128 code points in the base table, and 2 bytes for each character
+        // in the extended table. (The alphabet string has a space in the
+        // place where the escape char would be).
+        final int expectedEncodedLength = 128 + (2 * 9);
         final String alphabet =
             "@\u00a3$\u00a5\u00e8\u00e9\u00f9\u00ec\u00f2\u00c7\n\u00d8\u00f8"
             + "\r\u00c5\u00e5\u0394_\u03a6\u0393\u039b\u03a9\u03a0\u03a8\u03a3"
-            + "\u0398\u039e\u00c6\u00e6\u00df\u00c9 !\"#\u00a4%&\'()*+,-./012"
+            + "\u0398\u039e \u00c6\u00e6\u00df\u00c9 !\"#\u00a4%&\'()*+,-./012"
             + "3456789:;<=>?\u00a1ABCDEFGHIJKLMNOPQRSTUVWXYZ\u00c4\u00d6\u00d1"
             + "\u00dc\u00a7\u00bfabcdefghijklmnopqrstuvwxyz\u00e4\u00f6\u00f1"
             + "\u00fc\u00e0^{}\\[~]|\u20ac";
@@ -31,6 +32,13 @@ public class DefaultAlphabetEncodingTest extends TestCase {
         assertNotNull(decoded);
         assertEquals(expectedEncodedLength, encoded.length);
         assertEquals(alphabet, decoded);
+        
+        for (int i = 0; i < 128; i++) {
+            if (i == DefaultAlphabetEncoding.EXTENDED_ESCAPE) {
+                continue;
+            }
+            assertEquals((char) i, encoded[i]);
+        }
     }
     
     public void testPackUnder8ByteBoundary() throws Exception {
