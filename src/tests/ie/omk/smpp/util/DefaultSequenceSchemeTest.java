38b3a62dfaf0f1740bd13b4c245dc73b4830f841
Oran Kelly <oranoceallaigh@smppapi.git>
2007-02-18 18:40:20 +0000

    Sequence numbers returned are now longs. Add testWrap.

--- src/tests/ie/omk/smpp/util/DefaultSequenceSchemeTest.java
+++ src/tests/ie/omk/smpp/util/DefaultSequenceSchemeTest.java
@@ -6,30 +6,38 @@ public class DefaultSequenceSchemeTest extends TestCase {
 
     public void testNextNumber() {
         DefaultSequenceScheme seq = new DefaultSequenceScheme();
-        assertEquals(1, seq.nextNumber());
-        assertEquals(2, seq.nextNumber());
-        assertEquals(3, seq.nextNumber());
-        assertEquals(4, seq.nextNumber());
-        assertEquals(5, seq.nextNumber());
-        assertEquals(6, seq.nextNumber());
+        assertEquals(1L, seq.nextNumber());
+        assertEquals(2L, seq.nextNumber());
+        assertEquals(3L, seq.nextNumber());
+        assertEquals(4L, seq.nextNumber());
+        assertEquals(5L, seq.nextNumber());
+        assertEquals(6L, seq.nextNumber());
     }
     public void testPeek() {
         DefaultSequenceScheme seq = new DefaultSequenceScheme();
-        assertEquals(1, seq.peek());
-        assertEquals(1, seq.nextNumber());
-        assertEquals(2, seq.peek());
-        assertEquals(2, seq.peek());
-        assertEquals(2, seq.peek());
-        assertEquals(2, seq.peek());
-        assertEquals(2, seq.nextNumber());
-        assertEquals(3, seq.peek());
-        assertEquals(13, seq.peek(10));
+        assertEquals(1L, seq.peek());
+        assertEquals(1L, seq.nextNumber());
+        assertEquals(2L, seq.peek());
+        assertEquals(2L, seq.peek());
+        assertEquals(2L, seq.peek());
+        assertEquals(2L, seq.peek());
+        assertEquals(2L, seq.nextNumber());
+        assertEquals(3L, seq.peek());
+        assertEquals(13L, seq.peek(10L));
     }
     public void testReset() {
         DefaultSequenceScheme seq = new DefaultSequenceScheme();
-        while (seq.nextNumber() < 1450);
+        while (seq.nextNumber() < 1450L);
         assertEquals(1451, seq.peek());
         seq.reset();
         assertEquals(1, seq.nextNumber());
+    }
+
+    public void testWrap() {
+        DefaultSequenceScheme dss = new DefaultSequenceScheme(
+                DefaultSequenceScheme.MAX_VALUE - 1);
+        assertTrue(dss.nextNumber() == DefaultSequenceScheme.MAX_VALUE - 1L);
+        assertTrue(dss.nextNumber() == DefaultSequenceScheme.MAX_VALUE);
+        assertTrue(dss.nextNumber() == 1L);
     }
 }
