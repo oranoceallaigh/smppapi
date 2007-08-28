72a20f142b3e2fb66f22b31a089edf1b0a3a1537
Oran Kelly <oranoceallaigh@smppapi.git>
2007-08-28 16:43:21 +0000

    Add integer mappings for ConnectionState, for use with an AtomicInteger.

--- src/java/ie/omk/smpp/ConnectionState.java
+++ src/java/ie/omk/smpp/ConnectionState.java
@@ -5,8 +5,27 @@ package ie.omk.smpp;
  * @version $Id:$
  */
 public enum ConnectionState {
-    UNBOUND,
-    BINDING,
-    BOUND,
-    UNBINDING,
+    UNBOUND(0),
+    BINDING(1),
+    UNBINDING(2),
+    BOUND(3);
+    
+    private int state;
+    
+    private ConnectionState(int state) {
+        this.state = state;
+    }
+    
+    public int intValue() {
+        return state;
+    }
+    
+    public static final ConnectionState valueOf(int value) {
+        ConnectionState[] states = ConnectionState.values();
+        if (value >=0 && value < states.length) {
+            return states[value];
+        } else {
+            return null;
+        }
+    }
 }
