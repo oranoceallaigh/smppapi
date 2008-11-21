3b1b6a953921e4fa74861b7b1d4ae86697b79248
Oran Kelly <oranoceallaigh@smppapi.git>
2008-11-21 00:55:06 +0000

    Enhance the unit tests for the alphabet encodings.

--- src/test/java/ie/omk/smpp/encoding/DefaultAlphabetEncodingTest.java
+++ src/test/java/ie/omk/smpp/encoding/DefaultAlphabetEncodingTest.java
@@ -1,8 +1,8 @@
 package ie.omk.smpp.encoding;
 
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNotNull;
-import ie.omk.smpp.encoding.DefaultAlphabetEncoding;
+
+import java.io.UnsupportedEncodingException;
 
 import org.testng.annotations.Test;
 
@@ -10,41 +10,77 @@ import org.testng.annotations.Test;
  * @version $Id:$
  */
 @Test
-public class DefaultAlphabetEncodingTest {
+public class DefaultAlphabetEncodingTest extends BaseAlphabetEncodingTest<DefaultAlphabetEncoding> {
 
-    private final DefaultAlphabetEncoding encoding =
-        new DefaultAlphabetEncoding();
+    private static final String ALPHABET =
+        "@\u00a3$\u00a5\u00e8\u00e9\u00f9\u00ec"
+        + "\u00f2\u00c7\n\u00d8\u00f8\r\u00c5\u00e5"
+        + "\u0394_\u03a6\u0393\u039b\u03a9\u03a0\u03a8"
+        + "\u03a3\u0398\u039e\u00c6\u00e6\u00df\u00c9"
+        + " !\"#\u00a4%&'"
+        + "()*+,-./"
+        + "01234567"
+        + "89:;<=>?"
+        + "\u00a1ABCDEFG"
+        + "HIJKLMNO"
+        + "PQRSTUVW"
+        + "XYZ\u00c4\u00d6\u00d1\u00dc\u00a7"
+        + "\u00bfabcdefg"
+        + "hijklmno"
+        + "pqrstuvw"
+        + "xyz\u00e4\u00f6\u00f1\u00fc\u00e0"
+        + "^{}\\[~]|\u20ac";
+
+    private static final int[] BYTES = {
+        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+        0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+        0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+        0x18, 0x19, 0x1a, 0x1c, 0x1d, 0x1e, 0x1f,
+        0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27,
+        0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f,
+        0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37,
+        0x38, 0x39, 0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x3f,
+        0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47,
+        0x48, 0x49, 0x4a, 0x4b, 0x4c, 0x4d, 0x4e, 0x4f,
+        0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57,
+        0x58, 0x59, 0x5a, 0x5b, 0x5c, 0x5d, 0x5e, 0x5f,
+        0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67,
+        0x68, 0x69, 0x6a, 0x6b, 0x6c, 0x6d, 0x6e, 0x6f,
+        0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77,
+        0x78, 0x79, 0x7a, 0x7b, 0x7c, 0x7d, 0x7e, 0x7f,
+        0x1b, 0x14, 0x1b, 0x28, 0x1b, 0x29, 0x1b, 0x2f,
+        0x1b, 0x3c, 0x1b, 0x3d, 0x1b, 0x3e, 0x1b, 0x40,
+        0x1b, 0x65,
+    };
     
-    public void testDefaultAlphabet() {
-        // 128 code points in the base table, and 2 bytes for each character
-        // in the extended table. (The alphabet string has a space in the
-        // place where the escape char would be).
-        final int expectedEncodedLength = 128 + (2 * 9);
-        final String alphabet =
-            "@\u00a3$\u00a5\u00e8\u00e9\u00f9\u00ec\u00f2\u00c7\n\u00d8\u00f8"
-            + "\r\u00c5\u00e5\u0394_\u03a6\u0393\u039b\u03a9\u03a0\u03a8\u03a3"
-            + "\u0398\u039e \u00c6\u00e6\u00df\u00c9 !\"#\u00a4%&\'()*+,-./012"
-            + "3456789:;<=>?\u00a1ABCDEFGHIJKLMNOPQRSTUVWXYZ\u00c4\u00d6\u00d1"
-            + "\u00dc\u00a7\u00bfabcdefghijklmnopqrstuvwxyz\u00e4\u00f6\u00f1"
-            + "\u00fc\u00e0^{}\\[~]|\u20ac";
-        
-        byte[] encoded = encoding.encode(alphabet);
-        String decoded = encoding.decode(encoded);
-        
-        assertNotNull(encoded);
-        assertNotNull(decoded);
-        assertEquals(encoded.length, expectedEncodedLength);
-        assertEquals(decoded, alphabet);
-        
-        for (int i = 0; i < 128; i++) {
-            if (i == DefaultAlphabetEncoding.EXTENDED_ESCAPE) {
-                continue;
-            }
-            assertEquals(encoded[i], (char) i);
-        }
+    @Override
+    protected TestData getArrayToDecode() {
+        return new TestData(BYTES, ALPHABET);
     }
-    
+
+    @Override
+    protected DefaultAlphabetEncoding getEncodingToTest() throws UnsupportedEncodingException {
+        return new DefaultAlphabetEncoding();
+    }
+
+    @Override
+    protected TestData getFullySupportedStringToEncode() {
+        return new TestData(ALPHABET, BYTES);
+    }
+
+    @Override
+    protected TestData getPartiallySupportedStringToEncode() {
+        String string = "Unsupported character: \u010c";
+        int[] expectedBytes = new int[] {
+                85, 110, 115, 117, 112, 112, 111, 114,
+                116, 101, 100, 32, 99, 104, 97, 114,
+                97, 99, 116, 101, 114, 58, 32, 63,
+        };
+        return new TestData(expectedBytes, string);
+    }
+
     public void testPackUnder8ByteBoundary() throws Exception {
+        DefaultAlphabetEncoding encoding = getEncodingToTest();
         final byte[] testArray = new byte[] {
                 0x61, 0x62, 0x63, 0x64, 0x65, 0x66,
         };
@@ -57,6 +93,7 @@ public class DefaultAlphabetEncodingTest {
     }
     
     public void testPackOn8ByteBoundary() throws Exception {
+        DefaultAlphabetEncoding encoding = getEncodingToTest();
         final byte[] testArray = new byte[] {
                 0x2, 0x45, 0x12, 0x7e, 0x33, 0x66, 0x7f, 0x1a,
         };
@@ -75,6 +112,7 @@ public class DefaultAlphabetEncodingTest {
     }
 
     public void testPackOver8ByteBoundary() throws Exception {
+        DefaultAlphabetEncoding encoding = getEncodingToTest();
         final byte[] testArray = new byte[] {
                 0x02, 0x45, 0x12, 0x7e, 0x33, 0x66, 0x7f, 0x1a,
                 0x53, 0x7a, 0x47, 0x39, 0x78, 0x6f, 0x14, 0x4b,
@@ -105,6 +143,7 @@ public class DefaultAlphabetEncodingTest {
     }
     
     public void testUnpackUnder8ByteBoundary() throws Exception {
+        DefaultAlphabetEncoding encoding = getEncodingToTest();
         final byte[] testArray = new byte[] {
                 (byte) 0x61, (byte) 0xf1, (byte) 0x98, 0x5c, 0x36, 0x3,
         };
@@ -117,6 +156,7 @@ public class DefaultAlphabetEncodingTest {
     }
     
     public void testUnpackOn8ByteBoundary() throws Exception {
+        DefaultAlphabetEncoding encoding = getEncodingToTest();
         final byte[] testArray = new byte[] {
                 (byte) 0x82,
                 (byte) 0xa2,
@@ -135,6 +175,7 @@ public class DefaultAlphabetEncodingTest {
     }
     
     public void testUnpackOver8ByteBoundary() throws Exception {
+        DefaultAlphabetEncoding encoding = getEncodingToTest();
         final byte[] testArray = new byte[] {
                 (byte) 0x82,
                 (byte) 0xa2,
--- src/test/java/ie/omk/smpp/encoding/Latin1EncodingTest.java
+++ src/test/java/ie/omk/smpp/encoding/Latin1EncodingTest.java
@@ -1,9 +1,6 @@
 package ie.omk.smpp.encoding;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertNotNull;
-import ie.omk.smpp.encoding.Latin1Encoding;
+import java.io.UnsupportedEncodingException;
 
 import org.testng.annotations.Test;
 
@@ -11,36 +8,41 @@ import org.testng.annotations.Test;
  * @version $Id:$
  */
 @Test
-public class Latin1EncodingTest {
+public class Latin1EncodingTest extends BaseAlphabetEncodingTest<Latin1Encoding> {
     // "Test message" followed by:
     // Yen symbol
     // Pound sign (European interpretation, not what I would call a "hash").
     // Superscript 3
     // Latin capital letter AE
-    private final static byte[] EXPECTED_BYTES = {
-        0x54, 0x65, 0x73, 0x74,
-        0x20, 0x6d, 0x65, 0x73,
-        0x73, 0x61, 0x67, 0x65,
-        (byte) 0xa5, (byte) 0xa3, (byte) 0xb3, (byte) 0xc6,
+    private final static int[] EXPECTED_BYTES = {
+        0x54, 0x65, 0x73, 0x74, 0x20, 0x6d, 0x65, 0x73,
+        0x73, 0x61, 0x67, 0x65, 0xa5, 0xa3, 0xb3, 0xc6,
     };
     private static final String STRING = "Test message\u00a5\u00a3\u00b3\u00c6";
 
-    public void testLatinEncoding() throws Exception {
-        Latin1Encoding encoding = new Latin1Encoding();
-        byte[] encoded = encoding.encode(STRING);
-        String decoded = encoding.decode(encoded);
-        assertNotNull(encoded);
-        assertNotNull(decoded);
-        assertEquals(encoded, EXPECTED_BYTES);
-        assertEquals(decoded, STRING);
+    @Override
+    protected TestData getArrayToDecode() {
+        return new TestData(STRING, EXPECTED_BYTES);
     }
-    
-    public void testNonLatin1() throws Exception {
-        Latin1Encoding encoding = new Latin1Encoding();
-        String nonLatin1 = "\u20ac";
-        byte[] encoded = encoding.encode(nonLatin1);
-        String decoded = encoding.decode(encoded);
-        assertEquals(encoded.length, nonLatin1.length());
-        assertFalse(nonLatin1.equals(decoded));
+
+    @Override
+    protected Latin1Encoding getEncodingToTest() throws UnsupportedEncodingException {
+        return new Latin1Encoding();
+    }
+
+    @Override
+    protected TestData getFullySupportedStringToEncode() {
+        return new TestData(EXPECTED_BYTES, STRING);
+    }
+
+    @Override
+    protected TestData getPartiallySupportedStringToEncode() {
+        String string = "Unsupported character: \u20ac";
+        int[] expectedBytes = new int[] {
+                85, 110, 115, 117, 112, 112, 111, 114,
+                116, 101, 100, 32, 99, 104, 97, 114,
+                97, 99, 116, 101, 114, 58, 32, 63,
+        };
+        return new TestData(expectedBytes, string);
     }
 }
